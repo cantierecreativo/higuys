f12071d327680dbcd461c025f8bf8af8d7122c7e
Stefano Verna <s.verna@cantierecreativo.net>
2014-10-19 23:41:30 +0200

    Fix mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'hi_guys.com' }
+  config.action_mailer.default_url_options = { host: 'hi_guys.io' }
 end
 
